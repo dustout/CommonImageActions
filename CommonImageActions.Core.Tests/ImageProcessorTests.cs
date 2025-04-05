eb5054b16609987dad79da435a657874daccfc4e
Dustin <dustout@gmail.com>
2025-04-05 13:28:00 -0600

    Bringing in a basic unit test

--- CommonImageActions.Core.Tests/ImageProcessorTests.cs
+++ CommonImageActions.Core.Tests/ImageProcessorTests.cs
@@ -5,7 +5,36 @@ namespace CommonImageActions.Core.Tests
 {
     public class ImageProcessorTests
     {
-        
+        [Fact]
+        public async Task ProcessImageAsync_Stream_ShouldReturnProcessedImage()
+        {
+            var testJpg = Properties.Resources.testJpg;
+            var actions = new ImageActions()
+            {
+                Width = 100,
+            };
+            using var stream = new MemoryStream(testJpg);
+
+            // Act
+            var result = await ImageProcessor.ProcessImageAsync(stream, actions);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
+        }
+
+        [Fact]
+        public void GetInitials_ShouldReturnInitials()
+        {
+            // Arrange
+            var input = "John Doe";
+
+            // Act
+            var result = ImageProcessor.GetInitials(input);
+
+            // Assert
+            Assert.Equal("JD", result);
+        }
 
         [Fact]
         public void CalculateHash_ShouldReturnHash()
