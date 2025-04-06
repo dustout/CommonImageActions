c278852ab9e7df48e1fffe5f4bba1a7ee10d0362
Dustin <dustout@gmail.com>
2025-04-06 13:14:48 -0600

    Split off IsImage into a helper class

---
+++ CommonImageActions.Core.Tests/TestHelpers.cs
@@ -0,0 +1,19 @@
+﻿using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonImageActions.Core.Tests
+{
+    public static class TestHelpers
+    {
+        public static bool IsImage(byte[] imageData)
+        {
+            using var stream = new MemoryStream(imageData);
+            using var image = SKBitmap.Decode(stream);
+            return image != null;
+        }
+    }
+}
