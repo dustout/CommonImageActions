501c61bfbfdd2c389f34679ef1b1f9e538b95ff2
Dustin <dustout@gmail.com>
2025-04-12 08:15:28 -0600

    Added ImageProcessorException

---
+++ CommonImageActions.Core/ImageProcessorException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonImageActions.Core
+{
+    public class ImageProcessorException:Exception
+    {
+        public ImageProcessorException(string message)
+                    : base(message)
+        { }
+        public ImageProcessorException(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+    }
+}
