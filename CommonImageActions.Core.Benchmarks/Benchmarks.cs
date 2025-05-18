1196ce64c250c37d633fd3b4ce3c39fab19dae61
Dustin <dustout@gmail.com>
2025-05-17 23:14:52 -0600

    Added boilerplate benchmark project

---
+++ CommonImageActions.Core.Benchmarks/Benchmarks.cs
@@ -0,0 +1,21 @@
+﻿using BenchmarkDotNet;
+using BenchmarkDotNet.Attributes;
+using System;
+
+namespace CommonImageActions.Core.Benchmarks
+{
+    public class Benchmarks
+    {
+        [Benchmark(Baseline = true)]
+        public void Scenario1()
+        {
+            // Implement your benchmark here
+        }
+
+        [Benchmark]
+        public void Scenario2()
+        {
+            // Implement your benchmark here
+        }
+    }
+}
